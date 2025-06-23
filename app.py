8a4dba652f5909892ea1367eef2904bd01a39ded
glodirizzini <gabrielelodirizzini048@gmail.com>
2025-06-23 23:04:12 +0200

    Implemento runner windows e mac per far girare pipeline su github e permettere il rilascio automatico multipiattforma

--- app.py
+++ app.py
@@ -1,31 +1,10 @@
-# from flask import Flask, render_template, send_from_directory, jsonify
-# import os
-
-# app = Flask(__name__, static_folder="static", template_folder="templates_html")
-
-# TEMPLATES_DIR = "templates"
-
-# @app.route("/")
-# def index():
-#     return render_template("index.html")
-
-# @app.route("/list-templates")
-# def list_templates():
-#     files = [f for f in os.listdir(TEMPLATES_DIR) if f.endswith(".json")]
-#     return jsonify(files)
-
-# @app.route("/templates/<path:filename>")
-# def get_template_file(filename):
-#     return send_from_directory(TEMPLATES_DIR, filename)
-
-# if __name__ == "__main__":
-#     app.run(debug=True)
-
 from flask import Flask, render_template, request, jsonify, send_from_directory
 import os
 import sys
 import json
 import webbrowser
+
+APP_VERSION = "1.0.0"
 
 if getattr(sys, 'frozen', False):
     # Siamo in eseguibile generato da PyInstaller
@@ -56,6 +35,15 @@ def list_templates():
 def get_template(filename):
     with open(os.path.join(t_folder, filename), "r", encoding="utf-8") as f:
         return jsonify(json.load(f))
+    
+@app.route("/shutdown", methods=["POST"])
+def shutdown():
+    shutdown_server()
+    return "Server shutting down..."
+
+def shutdown_server():
+    print("Server shutting down...")
+    os._exit(0)
 
 if __name__ == "__main__":
     webbrowser.open("http://127.0.0.1:5000")  # Apre il browser automaticamente
